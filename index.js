f008d284033ddb16e8d59aecd85baff98ee993c5
Bronepuh <darckday2007@mail.ru>
2021-08-13 14:35:27 +0300

    fix

--- index.js
+++ index.js
@@ -165,9 +165,10 @@ app.use('/api/connection', routerConnection);
 // app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
-  });
+app.get('*', function (req, res) {
+  const index = path.join(__dirname, 'build', 'index.html');
+  res.sendFile(index);
+});
 
 async function start() {
   try {
